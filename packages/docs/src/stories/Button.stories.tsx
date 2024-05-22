c4f1bbcde472e730a8a570f33dbc6bc0b8b02988
jhameson <jhamesonlucas22@gmail.com>
2024-05-22 00:06:01 -0300

    feat: criando stories

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,18 @@
+import { Button, ButtonProps } from '@jl-ui/react'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Próximo',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
