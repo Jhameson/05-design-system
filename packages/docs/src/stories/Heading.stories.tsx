2e80180ab56df7bc2e68bfe703316d18fd8be0cd
jhameson <jhamesonlucas22@gmail.com>
2024-05-25 21:49:57 -0300

    feat: COMPONENTE HEADING

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,27 @@
+import { Heading, HeadingProps } from '@jl-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Heading',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão o heading sempre será um `H2` mas podemos alterar isso com a prop as',
+      },
+    },
+  },
+}
