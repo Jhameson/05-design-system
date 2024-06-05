e4979dc7b0fe6fd86aa572639f757edd525f1166
jhameson <jhamesonlucas22@gmail.com>
2024-06-05 00:10:22 -0300

    feat: componente checkBox

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,17 @@
+import { Check } from 'phosphor-react'
+import { CheckboxContainer, CheckboxIndicator } from './style'
+import { ComponentProps } from 'react'
+
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxContainer> {}
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      {/* asChild - funciona como fragmento */}
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
